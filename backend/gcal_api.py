ad143aae600731330095a93b6746cfec3448b2fe
Benjamin Sim <thatguyben@Benjamins-Laptop.local>
2025-03-02 19:23:51 -0600

    added docnotes for gcal_api

---
+++ backend/gcal_api.py
@@ -0,0 +1,12 @@
+"""
+File for communicated with the Google Calendar API. Functions here should be used for FocusBloom.
+Functions to implement:
+    - Schedule Task: based on parameters given by user, such as how long each wokr session would take, 
+    when it is due, priority, name of task. These can for now be implemented through command line
+        - Helper: look through calendar to find open timeslot
+    - Reschedule Task: User can reschedule task to add on to next available day before due date
+Data Structures: Should have a dictionary with task details so we can update accordingly with dates 
+    to work on, length of how long it should take, etc. Should update whenever a task is finished
+    
+
+"""
